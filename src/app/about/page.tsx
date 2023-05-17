9b580634982e1bc20bd3662b4d64550deb61116a
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2023-05-17 13:28:26 -0600

    test for pwa

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import { H1 } from '@/components/Headings';
+
+export default function About() {
+  return <H1>About</H1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { H1 } from '@/components/Headings';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className='w-screen h-screen grid place-items-center'>
-      <H1 className='text-cyan-500'>Peddler App</H1>
+      <div>
+        <H1 className='text-cyan-500'>Peddler App</H1>
+        <Link href={'/about'}>About</Link>
+      </div>
     </div>
   );
 }
